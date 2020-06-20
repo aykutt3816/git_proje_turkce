260ff4adcb62d29d4ab3b56a9b99a2c4a48c0006
Aykut <ata35314@gmail.com>
2020-06-20 21:27:50 +0200

    conflit olusturma cakıstırma

--- src/firstpackage/Firstclass.java
+++ src/firstpackage/Firstclass.java
@@ -6,7 +6,7 @@ public class Firstclass {
 		// TODO Auto-generated method stub
 System.out.println("first commit from local repostory");
 System.out.println("second commit from local repostory");
-System.out.println("third commit from local repostory");
+System.out.println("ucuncu commitimiz local repositryden");
 System.out.println("first commit from remote repostory");	
 	}
 
